30f758284e61ef3a89f8a91c4d8817b0915f489f
Saumya Bansal <saumyabansal17@gmail.com>
2025-11-03 23:51:32 +0530

    modified q2

--- Day2-code/q2/q2.js
+++ Day2-code/q2/q2.js
@@ -1,9 +1,18 @@
 var btn = document.getElementById("Button");
 
-btn.onmouseover = function() {
-    btn.style.backgroundColor = "blue"; 
-}
+btn.addEventListener("mouseover",function(){
+  this.style.backgroundColor = "blue"; 
+});
 
-btn.onmouseout = function() {
-  btn.style.backgroundColor = "lightblue"; 
-}
+btn.addEventListener("mouseout",function(){
+  this.style.backgroundColor = "lightblue"; 
+})
+
+//2nd solution
+// btn.onmouseover = function() {
+//     btn.style.backgroundColor = "blue"; 
+// }
+
+// btn.onmouseout = function() {
+//   btn.style.backgroundColor = "lightblue"; 
+// }
